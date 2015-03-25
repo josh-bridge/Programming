d7dedfcee502ac65f65f7f366ca285b3623b2bf7
Josh Bridge <14032908@asc-stu1253.ad.mmu.ac.uk>
2015-03-25 01:28:49 +0000

    Word Length Frequency Analysis
    whole buncha stuff for measuring word lengths to make way for word
    length frequencies

--- NewLetterFrequency.java
+++ NewLetterFrequency.java
@@ -74,8 +74,11 @@ public class NewLetterFrequency {
 			if(input.isEmpty() || input == null) {
 				System.out.println("-----Program Error - Empty or Invalid input: The program failed to run. It will restart.-----");
 			} else {
-				double alphabetFreq[] = analyzeText(input);
-				printOutput(alphabetFreq);
+				letterFrequency(input);
+				printWordAnalysis(input);
+				
+				System.out.println();
+				System.out.println("-------------------------------Output Ended------------------------------");
 			}
 			
 			System.out.println();
@@ -86,22 +89,23 @@ public class NewLetterFrequency {
 
 		System.out.println("===============================Program Quit==============================");
 	}
-
-	
-	
 	
     private static int totalPunctuation = 0;
 	private static int totalSpaces = 0;
 	private static int totalOtherChars = 0;
 	private static int zeroCount = 0;
-	private static int alphabet[] = new int[26];
 	
-	private static double[] analyzeText(String input) {
+	private static void letterFrequency(String input) {
+		int alphabet[] = countChars(input);
+		printOutput(alphabet, countToFreq(alphabet, input));
+	}
+	
+	private static int[] countChars(String input) {
 		//initialize punctuation variable
         
         //convert input to char array
         char charArr[] = input.toLowerCase().toCharArray();
-        
+        int alphabet[] = new int[26];
         int lettIndex;
         
         for (char c : charArr) {
@@ -117,25 +121,48 @@ public class NewLetterFrequency {
         	}
         }
 		
-		return countToFreq(input);
+		return alphabet;
 	}
 	
-	private static void clearAlphabet (){
-		int count = 0;
-		for(int i : alphabet) {
-			if (i > 0)
-				alphabet[count] = 0;
+
+	
+	private static int[] removeDuplicates(int input[]) {
+		Set<Integer> mySet = new HashSet<Integer>();
+		
+		for(int x : input) {
+			mySet.add((Integer) x);
+		}
+		
+		int output[] = new int[mySet.size()], count = 0;
+		
+		for(Integer y : mySet) {
+			output[count] = (int) y;
 			count++;
 		}
 		
-		totalPunctuation = 0;
-		totalSpaces = 0;
-		totalOtherChars = 0;
-		zeroCount = 0;
+		return output;
 	}
 	
+	private static int[] countOccurences(int inputA[], int inputB[]) {
+		
+		
+		//count how many of each num in output array are in the input array
+				int output[] = new int[inputA.length];
+				int count = 0;
+				
+				
+				for(int x : inputA) {
+					for(int z=0; z < inputB.length; z++) {
+						if(inputB[z] == x)
+							output[count]++;
+					}
+					count++;
+				}
+				
+		return output;
+	}
 	
-	private static double[] countToFreq (String input) {
+	private static double[] countToFreq (int alphabet[], String input) {
 		int count = 0;
         
         double output[] = new double [alphabet.length];
@@ -153,37 +180,6 @@ public class NewLetterFrequency {
         }
 		
 		return output;
-	}
-	
-	private static void printOutput (double input[]) {
-		printFreqs(input);
-		System.out.println();
-        toBarChart(input);
-
-		clearAlphabet();
-		
-
-        System.out.println();
-		System.out.println("------------------------------Program Ended------------------------------");
-	}
-	
-	private static void printFreqs (double input[]) {
-		 System.out.println(); //clear a line for the output
-
-			int count = 0;
-	        for (int b : alphabet) {
-	        	if (b > 0)
-	        		System.out.println(((char) (count + 'a')) + ": " + input[count] + " (" + b + " occurances)");
-	        	count++;
-	        }
-	        
-	        if (zeroCount > 0) {
-	     		System.out.println();
-	     		System.out.println(zeroCount + " letters were omitted (they occured zero times)");
-	        }
-     		System.out.println();
-	        System.out.println("Punctuation count: " + totalPunctuation);
-     	
 	}
 	
 	private static String readFile (String fileName) {
@@ -208,6 +204,155 @@ public class NewLetterFrequency {
 			System.out.println();
 			return "";
 		}
+	}
+	
+	public static void printWordAnalysis(String input) {
+		int lengths[] = wordLengths(input);
+		int sortedLengths[] = sortIntArray(lengths);
+		
+		System.out.println();
+		System.out.println("Mean Word Length: " + meanLength(lengths));
+		System.out.println("Median Word Length: " + medianLength(sortedLengths));
+		System.out.println("Mode Word Length: " + modeLength(sortedLengths));
+		
+		int inputNoDuplicates[] = removeDuplicates(sortedLengths);
+		int wordLengthOccurences[] = countOccurences(inputNoDuplicates, sortedLengths);
+		int count = 0;
+		
+		for(int i : wordLengthOccurences) {
+			System.out.println(inputNoDuplicates[count] + ": " + i + " occurences");
+			count++;
+		}
+		
+		
+	}
+	
+	
+	private static int[] wordLengths(String input) {
+		//run through and remove everything that's not a letter
+		//make sure only one space between words
+		//split string via " "
+		//put each length into separate output array
+		input = input.replace(System.getProperty("line.separator"), " ");
+		
+		char charArr[] = input.toLowerCase().toCharArray();
+        String finalString = "";
+        
+        for (char c : charArr) {
+        	if(isAtoZ(c) || c == ' ' ) {
+        		finalString += c;
+        	}
+        }
+        //make sure only one space between words using regex
+        //doing it after is better as removing punctuation may leave double spaces
+        finalString = finalString.replaceAll("\\s+", " ");
+        
+        String words[] = finalString.split(" ");
+        int count = 0, lengths[] = new int [words.length];
+        
+        for(String x : words) {
+        	lengths[count] = x.length();
+        	count++;
+        }
+		
+		return lengths;
+	}
+	
+	private static double meanLength(int input[]) {
+		int total = 0;
+		for(int i : input) {
+			total += i;
+		}
+		
+		double result = (Math.round(((double) total / (double) input.length) * 100.0) / 100.0);
+		
+		return result;
+	}
+	
+	private static double medianLength(int input[]) {
+		
+		double median;
+		if (input.length % 2 == 0) {
+			// even number of words
+			median = 0.5 * (input[input.length / 2] + input[input.length / 2 + 1]);
+		} else
+			// odd number of words
+			median = input[input.length / 2 + 1];
+		return median;
+
+	}
+	
+	private static int[] sortIntArray(int[] input) {
+
+		int output[] = new int[input.length];
+		for (int i = 0; i < input.length; i++)
+			output[i] = input[i];
+		
+		boolean swapped;
+		int max = output.length;
+		do {
+			swapped = false;
+			for (int i = 1; i < max; i++) {
+				if (output[i - 1] > output[i]) {
+					swap(output, i - 1, i);
+					swapped = true;
+				}
+			}
+			max--;
+		} while (swapped);
+		return output;
+
+	}
+	
+    public static int modeLength(int input[]) {
+        int modeLen = 0, bigCount = 0, countNum = 1, count = 0, prevLen = 0;
+
+        for(int l : input) {
+            if(prevLen == l && countNum > bigCount && count > 0) {
+                countNum++;
+                modeLen = prevLen;
+                bigCount = countNum;
+            } else if (prevLen != l & count > 0) 
+                countNum = 1;
+            else if (count == 0)
+                modeLen = l;
+            count++;
+            prevLen = l;
+        }
+        return modeLen;
+    }
+	
+	private static void swap(int[] input, int a, int b) {
+		int tmp = input[a];
+		input[a] = input[b];
+		input[b] = tmp;
+	}
+	
+	private static void printOutput (int alphabet[], double input[]) {
+		printFreqs(alphabet, input);
+		System.out.println();
+        toBarChart(input);
+
+        clearGlobalInts();
+
+	}
+	
+	private static void printFreqs (int alphabet[], double input[]) {
+		System.out.println(); //clear a line for the output
+
+		int count = 0;
+        for (int b : alphabet) {
+        	if (b > 0)
+        		System.out.println(((char) (count + 'a')) + ": " + input[count] + " (" + b + " occurances)");
+        	count++;
+        }
+        System.out.println();
+        if (zeroCount > 0) {
+     		System.out.println(zeroCount + " letters were omitted (they occured zero times)");
+     		System.out.println();
+        }
+        
+        System.out.println("Punctuation count: " + totalPunctuation);
 	}
 	
     private static boolean isAtoZ(char input) {
@@ -279,9 +424,18 @@ public class NewLetterFrequency {
             System.out.print(' ');
     }
 	
-    private static void spaces(double x, double y) {
-        int spaces = (int) (String.valueOf(y).length()) - (String.valueOf(x).length());
+    private static void spaces(double x, double max) {
+        int spaces = (int) (String.valueOf(max).length()) - (String.valueOf(x).length());
         space(spaces);
     }
+    
+	
+	private static void clearGlobalInts (){
+		totalPunctuation = 0;
+		totalSpaces = 0;
+		totalOtherChars = 0;
+		zeroCount = 0;
+	}
+	
 
 }
